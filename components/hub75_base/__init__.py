8516248ac03b576fe5a21f64c3de9fd1f3cf80a0
ntableman <nathan@tableman.com>
2024-09-05 18:27:43 -0700

    start lib update

--- components/hub75_base/__init__.py
+++ components/hub75_base/__init__.py
@@ -169,5 +169,5 @@ async def setup_hub75_display(var, config):
     cg.add_library("Wire", None)
     cg.add_library("Adafruit BusIO", None)
     cg.add_library("Adafruit GFX Library", None)
-    cg.add_library("ESP32 HUB75 LED MATRIX PANEL DMA Display", "2.0.7")
-
+    #cg.add_library("ESP32 HUB75 LED MATRIX PANEL DMA Display", "2.0.7")
+    cg.add_library("ESP32 HUB75 LED MATRIX PANEL DMA Display", None)
